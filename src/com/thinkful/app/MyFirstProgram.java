41c6a972a9cb90c3262d544890148886ee537af9
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-06-27 22:31:21 -0700

    first commit

---
+++ src/com/thinkful/app/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package com.thinkful.app;
+
+public class MyFirstProgram{
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        for (String s: args) {
+            System.out.println(s);
+
+        }
+    }
+}
